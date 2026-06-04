06206326ac7f4adb5edde6e775b9d59bd604aa98
Nikita Sharapatov <qum1ch@mail.ru>
2026-06-04 20:51:18 +0300

    1.6 update rustore release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.f1app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.5"
+        versionCode = 12
+        versionName = "1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
